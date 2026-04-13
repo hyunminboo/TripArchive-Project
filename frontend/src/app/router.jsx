3c1d3cfdf4e5e652e0339ecd35ef285eb71fd2ed
hyunminboo <boohyunmin@gmail.com>
2026-04-13 21:14:16 +0900

    frontend update

--- frontend/src/app/router.jsx
+++ frontend/src/app/router.jsx
@@ -5,13 +5,13 @@ import PublicLayout from './PublicLayout'
 import Landing from '../pages/landing/Landing'
 import Login from '../pages/auth/Login'
 import Signup from '../pages/auth/Signup'
-// import ProtectRoute from '../store/ProtectRoute'
-// import PostDashboard from '../pages/posts/PostDashboard'
-// import PostAll from '../pages/posts/PostAll'
-// import PostEdit from '../pages/posts/PostEdit'
-// import PostCreate from '../pages/posts/PostCreate'
-// import PostDetail from '../pages/posts/PostDetail'
-import Setting from '../pages/setting/Setting'
+import ProtectRoute from '../store/ProtectRoute'
+import PostDashboard from '../pages/posts/PostDashboard'
+import PostAll from '../pages/posts/PostAll'
+import PostEdit from '../pages/posts/PostEdit'
+import PostCreate from '../pages/posts/PostCreate'
+import PostDetail from '../pages/posts/PostDetail'
+// import Setting from '../pages/setting/Setting'
 // import Profile from '../pages/profile/Profile'
 export const router = createBrowserRouter([
   {
@@ -23,21 +23,21 @@ export const router = createBrowserRouter([
       { path: '/signup', element: <Signup /> }
     ]
   }, {
-    // // 보호영역 부분
-    // path: '/app',
-    // element: (
-    //   <ProtectRoute>
-    //     <ProtectApp />
-    //   </ProtectRoute>
-    // ),
-    // children: [
-    //   { index: true, element: <PostDashboard /> },
-    //   { path:'posts/all', element: <PostAll /> },
-    //   { path:'posts/new', element: <PostCreate /> },
-    //   { path:'posts/:id', element: <PostDetail /> },
-    //   { path:'posts/:id/edit', element: <PostEdit /> },
-    //   // { path:'profile', element: <Profile /> },
-    //   { path:'setting', element: <Setting /> },
-    // ]
+    // 보호영역 부분
+    path: '/app',
+    element: (
+      <ProtectRoute>
+        <ProtectApp />
+      </ProtectRoute>
+    ),
+    children: [
+      { index: true, element: <PostDashboard /> },
+      { path:'posts/all', element: <PostAll /> },
+      { path:'posts/new', element: <PostCreate /> },
+      { path:'posts/:id', element: <PostDetail /> },
+      { path:'posts/:id/edit', element: <PostEdit /> },
+      // { path:'profile', element: <Profile /> },
+      // { path:'setting', element: <Setting /> },
+    ]
   }
 ])
